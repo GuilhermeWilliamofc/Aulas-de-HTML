8b7948ad54adb997e9d1a7345aab1134363a97ce
Guilherme William <desouzarodrigueswilliam@gmail.com>
2025-07-09 23:08:14 -0300

    Scripts do Dia 09 de Julho de 2025

---
+++ Curso em Vídeo/Desafios/Módulo 4/Desafio 14/js/script.js
@@ -0,0 +1,9 @@
+function ajustartela0(){
+    var iframe = document.getElementById('tela')
+    iframe.style.height = "507px"
+}
+
+function ajustartela1(){
+    var iframe = document.getElementById('tela')
+    iframe.style.height = "480px"
+}
